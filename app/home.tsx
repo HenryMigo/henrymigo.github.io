a66e8163b3934aa1e782e2b129e4c85ec09fc5c7
Henry Migo <henryeamigo@gmail.com>
2024-10-05 16:10:39 +0100

    Remove button and update packages

--- app/home.tsx
+++ app/home.tsx
@@ -16,31 +16,6 @@ export default function Home() {
           new things.
         </p>
       </div>
-      <div className="text-center px-10 pb-10">
-        <button
-          type="button"
-          className="text-gray-600 border-grey-600 focus:ring-4 focus:outline-none focus:ring-gray-600 
-                           font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 border-2 border-gray-600
-                           dark:border-white dark:text-white dark:hover:text-white dark:focus:ring-white"
-        >
-          <svg
-            className="w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-          <span className="sr-only">Icon description</span>
-        </button>
-      </div>
       <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white">
         <a href="https://github.com/HenryMigo" target="_blank" rel="noreferrer">
           <AiFillGithub />
